52fbfc6d033970a2f713cd5384cac29b4c13c3a0
Zoltan Mark Pinter <zoltan.pinter@danfoss.com>
2019-03-11 21:27:04 +0100

    Linearization modification started for the gas loop

--- Linearization/linearization.m
+++ Linearization/linearization.m
@@ -1,73 +1,90 @@
 clearvars
 close all
-syms KvV KvG TauV TauG TauMT TauA R VGC VR VC VMT DdDp1 DdDh1 DdDp2 DdDh2 DdDpR DdDhR DdDpC DdDhC eS DdDpMT DdDhMT DhDpMT
-syms p1 p2 pR pC h1 h2 hMT hL hG hR hC hCi hF d1 d2 dR dG dC dMT dA DQC DQF CRV CRG fMT fA DVA DmV DmMT DmG DmL DmCi DmCo DmLT DmF DVA
-syms s s0 k cp TA1 TA2 T1 T2 TA0 DTDp1 DTDh1 DTDp2 DTDh2 MxDVA
+syms KvHP KvG                           % Kv value of valve
+syms TauHP TauG TauVA TauBP TauBC TauR TauTA TauIT  % Time constants
+syms R                                  % Hydraulic resistance
+syms Vc VR VIT                          % Volumes of cells, reseiver and piston
+syms DdDp1 DdDh1 DdDp2 DdDh2 DdDpR DdDhR DdDpBP DdDhBP
+syms eS                                 % Isentropic efficiency
+syms p1 p2 pR pMT                       % Pressures
+syms h1 h2 hL hG hR hMT hHR hC hF       % Enthalpies
+syms d1 d2 dR dG dA dBP                 % Densities
+syms CRHP CRG BPR BP                    % Capacity or division ratios
+syms fIT fA                             % Frequencies
+syms DmHP DmIT DmG DmMT Dm21            % Mass flow rates
+syms DVA                                % Volume flow rate of air
+syms s s0 k                             % Convection parameters
+syms cp                                 % Specific heat capacity of air
+syms TA1 TA2 T1 T2 TBP TA0              % Temperatures
+syms DTDp1 DTDh1 DTDp2 DTDh2 DTDpBP DTDhBP
+syms DhDpIT                             % Isentropic curve delta ratio
+syms DQC DQF                            % Heat flow rates
+syms MxDVA                              % Maximal volume flow of fan
+syms delta_hHR delta_h2
 % ----------------------- STATIC EQUATIONS --------------------------------
+% Note the parameters for identification TODO hL dBP dIT
+% Boundary condition
+DmC = DQC/(hC-hL);
+DmF = DQF/(hF-hL);
+DmL = DmC + DmF;
+% Compressor
+hIT = hG + DhDpIT*(p1 - pR)/eS;
+% Heat recovery
+DmHR = DmMT + DmIT;
+hHR = (hMT*DmMT+hIT*DmIT)/DmHR - delta_hHR;
 % Gas cooler, heat transfer
-s = 1/(1/(s0 + k*DVA) + 1/(dA*DVA*cp));
+s = s0 + k*DVA;
 w = dA*DVA*cp/s;
-TA1 = 1/(w+1)*T1 + 1/(1/w+1)*TA0;
-TA2 = 1/(w+1)*T2 + 1/(1/w+1)*TA1;
 DQ1 = (TA1 - T1)*s;
 DQ2 = (TA2 - T2)*s;
-% Receiver valve: density is set as constant
-% Freezer BC 
-DmF = DQF/(hF-hL);
-% MT Joint 
-DmCo = - DmG + DmMT - DmF;
-hMTi = (DmG*hG + DmCo*hC + DmF*hF)/DmMT;
-% Compressor
-hMT = hMTi + DhDpMT*(p1 - pC)/eS;
-% Cooler volume
-DmCi = DQC/(hCi-hL);
-JoiningC = [-1 dC; DdDpC DdDhC];
-DPsiC = 1/VC*(DmCi*hCi-DmCo*hC);
-% Receiver Joint
-DmL = DmF + DmCi;
-% Receiver
-JointR = [-1 dR; DdDpR DdDhR];
-DPsiR = 1/VR*(DmV*h2 - DmL*hL - DmG*hG);
 % Gas cooler, fluid side
-Dm21 = 1/R*sqrt(d1*(p1-p2));
 Joining1 = [-1 d1; DdDp1 DdDh1];
 Joining2 = [-1 d2; DdDp2 DdDh2];
-DPsi1 = 1/VGC*(DmMT*hMT-Dm21*h1 + DQ1);
-DPsi2 = 1/VGC*(Dm21*h1-DmV*h2 + DQ2);
-% Fan (A refers to air)
-% ---------------------- DYNAMIC EQUATIONS --------------------------------
+DmBP = DmHP*BP;
+Dm1 = DmHR - DmBP;
+Dm2 = DmHP*(1-BP);
+DPsi1 = Dm1*hHR-Dm21*h1 + DQ1;
+DPsi2 = Dm21*h1-Dm2*h2 + DQ2;
+% ByPass valve
+hBP = (Dm2*h2 + DmBP*hHR)/DmHP;
 % High pressure valve
-DDmV = 1/TauV*(-DmV + CRV*KvV*sqrt(d2*(p2 - pR)));
+% Receiver
+JointR = [-1 dR; DdDpR DdDhR];
+DPsiR = 1/VR*(DmHP*hBP - DmL*hL - DmG*hG);
+% Receiver Valve
+% Fan
+% ---------------------- DYNAMIC EQUATIONS --------------------------------
+% Boundary condition
+DDmMT = 1/TauBC*(-DmMT + DmL);
+% Heat Recovery (no differentials)
 % Gas cooler
-Dd1 = 1/VGC*(DmMT-Dm21);
-Dd2 = 1/VGC*(Dm21-DmV);
-Dph1 = Joining1\[DPsi1; Dd1];
-Dph2 = Joining2\[DPsi2; Dd2];
+DDm21 = 1/TauR*(-Dm21 + 1/R*sqrt(d1*(p1-p2)));
+Dd1 = 1/Vc*(Dm1-Dm21);
+Dd2 = 1/Vc*(Dm21-DmHP*(1-BP));
+Dph1 = Joining1\[DPsi1/Vc; Dd1];
+Dph2 = Joining2\[DPsi2/Vc; Dd2];
 DT1 = [DTDp1 DTDh1]*Dph1;
 DT2 = [DTDp2 DTDh2]*Dph2;
-% Receiver Joint
+DTA1 = 1/TauTA*(-TA1 + 1/(w+1)*T1 + 1/(1/w+1)*TA0);
+DTA2 = 1/TauTA*(-TA2 + 1/(w+1)*T2 + 1/(1/w+1)*TA1);
+% ByPass Valve
+DBP = 1/TauBP*(-BP + BPR);
+% High pressure valve
+DDmHP = 1/TauHP*(-DmHP + CRHP*KvHP*sqrt(dBP*(p2 - pR)));
 % Receiver
-DdR = 1/VR*(DmV-DmL-DmG);
+DdR = 1/VR*(DmHP-DmL-DmG);
 DphR = JointR\[DPsiR; DdR];
-% Receiver valve: density is set as constant
-DDmG = 1/TauG*(-DmG + CRG*KvG*sqrt(dG*(pR - pC)));
-% Freezer BC 
+% Receiver valve
+DDmG = 1/TauG*(-DmG + CRG*KvG*sqrt(dG*(pR - pMT)));
 % Compressor
-DDmMT = 1/TauMT*(-DmMT + dMT*VMT*fMT);
-% Cooler volume
-DdC = 1/VC*(DmCi-DmCo);
-DphC = JoiningC\[DPsiC; DdC];
-% MT Joint 
-DpMT = DphC(1);
-DhMTi = DphC(2);
-DdMT = [DdDpMT DdDhMT]*[DpMT; DhMTi];
+DDmIT = 1/TauIT*(-DmIT + dG*VIT*fIT);
 % Fan (A refers to air)
-DDVA = 1/TauA*(-DVA + dA*MxDVA*fA);
+DDVA = 1/TauVA*(-DVA + dA*MxDVA*fA);
 % ------------------------ LINEARIZATION ----------------------------------
 % Augmentation
-Dx = [DDVA; Dph1; Dd1; DT1; Dph2; Dd2; DT2; DDmV; DphR; DdR; DDmG; DphC; DdC; DdMT; DDmMT];
-x = [DVA; p1; h1; d1; T1; p2; h2; d2; T2; DmV; pR; hR; dR; DmG; pC; hC; dC; dMT; DmMT];
-u = [fA; CRV; CRG; fMT];
+Dx = [DDVA; Dph1; Dd1; DT1; Dph2; Dd2; DT2; DDmHP; DphR; DdR; DDmG; DphC; DdC; DdMT; DDmMT];
+x = [DVA; p1; h1; d1; T1; p2; h2; d2; T2; DmHP; pR; hR; dR; DmG; pC; hC; dC; dMT; DmMT];
+u = [fA; CRHP; CRG; fMT];
 d = [dA; TA0; DmMT; hL; hG; dG; hCi; hF; DQC; DQF];
 % Jacobians
 A = jacobian(Dx,x);
@@ -78,12 +95,12 @@ A = subs(A,{p1 p2 pR pC},num2cell([86 84.8 38 30]*10^5));
 A = subs(A,{h1 h2 hMT hL hG hR hC hCi hF},num2cell([450 350 525 212 430 300 460 460 460]*10^3));
 A = subs(A,{d1 d2 dR dG dC dMT dA},num2cell([280 700 220 100 80 80 1.25]));
 A = subs(A,{TA0 TA1 TA2 T1 T2},num2cell([30 35 40 90 50]+273.15));
-A = subs(A,{DmV DmMT DmG DmL DmCi DmCo DmLT DmF},num2cell([0.321 0.321 0.123 0.198 0.151 0.151 0.046 0.046]));
+A = subs(A,{DmHP DmMT DmG DmL DmCi DmCo DmLT DmF},num2cell([0.321 0.321 0.123 0.198 0.151 0.151 0.046 0.046]));
 A = subs(A,{DVA MxDVA},{3.33 6.66});
-A = subs(A,{CRV CRG fMT fA},num2cell([0.25 0.25 0.25 0.6]));
+A = subs(A,{CRHP CRG fMT fA},num2cell([0.25 0.25 0.25 0.6]));
 A = subs(A,{DQC DQF},num2cell([36 11]*10^3));
-A = subs(A,{KvV KvG},num2cell([0.8 2]*8.7841e-06));
-A = subs(A,{TauV TauG TauMT TauA},num2cell([0.1 0.1 1 1]*10^0));
+A = subs(A,{KvHP KvG},num2cell([0.8 2]*8.7841e-06));
+A = subs(A,{TauHP TauG TauMT TauVA},num2cell([0.1 0.1 1 1]*10^0));
 A = subs(A,{R},num2cell(1.5*10^5));
 A = subs(A,{eS},num2cell(0.6));
 A = subs(A,{s s0 k cp},num2cell([1000 200 900 1000]));
@@ -96,7 +113,7 @@ A = subs(A,{DhDpMT},num2cell(0.01));
 A = double(A);
 % ----------------------- POSTPROCESSING ----------------------------------
 % Normalizing with maximum deviations
-% x = [DVA; p1; h1; d1; T1; p2; h2; d2; T2; DmV; pR; hR; dR; DmG; pC; hC; dC; dMT; DmMT];
+% x = [DVA; p1; h1; d1; T1; p2; h2; d2; T2; DmHP; pR; hR; dR; DmG; pC; hC; dC; dMT; DmMT];
 cond(A)
 DVBound = 2; 
 pBound = 20*10^5;
