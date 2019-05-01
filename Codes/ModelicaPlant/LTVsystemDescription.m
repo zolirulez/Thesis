95cb4c32a726613fb01023734da8504624a8d5ba
Zoltan Mark Pinter <pinter@danfoss.com>
2019-05-01 15:36:24 +0200

    LTV estimator is stable, but needs tuning

--- Codes/ModelicaPlant/LTVsystemDescription.m
+++ Codes/ModelicaPlant/LTVsystemDescription.m
@@ -1,87 +1,102 @@
-function ABCD = LTVsystemDescription(u,x)
+function ABCD = LTVsystemDescription(u,x,y,feedback)
 
-% p2Value = y(1);
-% hBPValue = y(2);
-% pRValue = y(3);
-% hRValue = y(4);
-% hHRValue = y(5);
-% % TA1Value = y(6);
-% DmQValue = y(7);
+% Use x or y (x means feedback, y means rough assumptions)
+% feedback = 0;
 
-constants = load('constants.mat')
+constants = load('constants.mat');
 constantfields = fieldnames(constants);
 for i=1:length(constantfields)
     eval([constantfields{i} '=constants.' constantfields{i} ';']);
 end
 
-if 1%~all(u==0)
-    % Input values
-    CRAValue = u(1);
-    BPRValue = u(2);
-    CRVValue = u(3);
-    CRITValue = u(4);
-    delta_hHRValue = u(5);
-    dBPValue = u(6);
-    dGValue = u(7);
-    hGValue = u(8);
-    hLValue = u(9);
-    TA0Value = u(10);
-    hMTValue = u(11);
-    % State values
-    DVAValue = initx(1);
-    p1Value = initx(2); % p1Value = initx(2);
-    h1Value = initx(3);
-    d1Value = initx(4);
-    TA2Value = initx(5); % TA2Value = initx(5);
-    Dm21Value = initx(6);
-    p2Value = initx(7);
-    h2Value = initx(8);
-    d2Value = initx(9);
-    TA1Value = initx(10);
-    BPValue = initx(11); % BPValue = initx(11);
-    DmVValue = initx(12);
-    pRValue = initx(13);
-    hRValue = initx(14);
-    dRValue = initx(15);
-    DmGValue = initx(16);
-    delta_h2Value = initx(17);
-    DmQValue = initx(18);    
-else
-    % Input values
-    CRAValue = initu(1);
-    BPRValue = initu(2);
-    CRVValue = initu(3);
-    CRITValue = initu(4);
-    delta_hHRValue = initu(5);
-    dBPValue = initu(6);
-    dGValue = initu(7);
-    hGValue = initu(8);
-    hLValue = initu(9);
-    TA0Value = initu(10);
-    hMTValue = initu(11);
-    % State values
-    DVAValue = initx(1);
-    p1Value = initx(2); % p1Value = initx(2);
-    h1Value = initx(3);
-    d1Value = initx(4);
-    TA2Value = initx(5); % TA2Value = initx(5);
-    Dm21Value = initx(6);
-    p2Value = initx(7);
-    h2Value = initx(8);
-    d2Value = initx(9);
-    TA1Value = initx(10);
-    BPValue = initx(11); % BPValue = initx(11);
-    DmVValue = initx(12);
-    pRValue = initx(13);
-    hRValue = initx(14);
-    dRValue = initx(15);
-    DmGValue = initx(16);
-    delta_h2Value = initx(17);
-    DmQValue = initx(18);
-end
+% Input values
+CRAValue = u(1);
+BPRValue = u(2);
+CRVValue = u(3);
+CRITValue = u(4);
+delta_hHRValue = u(5);
+dBPValue = u(6);
+dGValue = u(7);
+hGValue = u(8);
+hLValue = u(9);
+TA0Value = u(10);
+hMTValue = u(11);
 
-% Table values
-try
+
+if ~feedback
+    p2Value = y(1);
+    hBPValue = y(2);
+    pRValue = y(3);
+    hRValue = y(4);
+    hHRValue = y(5);
+    TA1Value = y(6);
+    DmQValue = y(7);
+    
+    % Used state values
+    DVAValue = c.MxDVAValue*CRAValue;
+    p1Value = p2Value + 1.5e5;
+    h1Value = (hBPValue + initx(17) + hHRValue)/2;
+    d1Value = initx(4);
+    sValue = c.s0Value + c.kValue*DVAValue;
+    wValue = c.dAValue*DVAValue*c.cpValue/sValue;
+    h2Value = hBPValue + initx(17);
+    
+    % Table values
+    try
+        p1idx = [find(p1Value < Pbig)-1 50];
+        p2idx = [find(p2Value < Pbig)-1 50];
+        pRidx = [find(pRValue < Pbig)-1 50];
+        h1idx = [find(h1Value < Hbig)-1 50];
+        h2idx = [find(h2Value < Hbig)-1 50];
+        hRidx = [find(hRValue < Hbig)-1 50];
+        delta_ph1Value = paramvectorP(1,p1idx(1),h1idx(1));
+        delta_ph2Value = paramvectorP(1,p2idx(1),h2idx(1));
+        delta_phRValue = paramvectorP(1,pRidx(1),hRidx(1));
+        delta_pd1Value = paramvectorP(2,p1idx(1),h1idx(1));
+        delta_pd2Value = paramvectorP(2,p2idx(1),h2idx(1));
+        delta_pdRValue = paramvectorP(2,pRidx(1),hRidx(1));
+        delta_Th1Value = paramvectorT(1,p1idx(1),h1idx(1));
+        delta_Th2Value = paramvectorT(1,p2idx(1),h2idx(1));
+        delta_Td1Value = paramvectorT(2,p1idx(1),h1idx(1));
+        delta_Td2Value = paramvectorT(2,p2idx(1),h2idx(1));
+    catch
+        disp('stop here')
+    end
+    % State values
+    TA2Value = 1/(wValue+1)*(delta_Th1Value*h1Value + delta_Td1Value*d1Value) + 1/(1/wValue+1)*TA1Value;
+    Dm21Value = 1/c.RValue*sqrt(d1Value*(p1Value-p2Value));
+    %     p2Value = initx(7);
+    d2Value = initx(9);
+    %     TA1Value = initx(10);
+    BPValue = BPRValue;
+    DmVValue = CRVValue*c.KvVValue*sqrt(dBPValue*(p2Value - pRValue));
+    %     pRValue = initx(13);
+    %     hRValue = initx(14);
+    dRValue = initx(15);
+    DmGValue = dGValue*c.VGValue*CRITValue*c.MxfITValue;
+    delta_h2Value = initx(17);
+    %     DmQValue = initx(18);
+else
+    % States
+    DVAValue = x(1);
+    p1Value = x(2);
+    h1Value = x(3);
+    d1Value = x(4);
+    TA2Value = x(5);
+    Dm21Value = x(6);
+    p2Value = x(7);
+    h2Value = x(8);
+    d2Value = x(9);
+    TA1Value = x(10);
+    BPValue = x(11);
+    DmVValue = x(12);
+    pRValue = x(13);
+    hRValue = x(14);
+    dRValue = x(15);
+    DmGValue = x(16);
+    delta_h2Value = x(17);
+    DmQValue = x(18);
+    % Tables
     p1idx = [find(p1Value < Pbig)-1 50];
     p2idx = [find(p2Value < Pbig)-1 50];
     pRidx = [find(pRValue < Pbig)-1 50];
@@ -98,9 +113,8 @@ try
     delta_Th2Value = paramvectorT(1,p2idx(1),h2idx(1));
     delta_Td1Value = paramvectorT(2,p1idx(1),h1idx(1));
     delta_Td2Value = paramvectorT(2,p2idx(1),h2idx(1));
-catch
-    disp('stop here')
 end
+
 
 % Substitutions
 value = [strrep('BP,CRIT,CRV,DVA,Dm21,DmG,DmQ,DmV,TA0,TA1,TA2,d1,d2,dBP,dG,dR,delta_h2,delta_Td1,delta_Td2,delta_Th1,delta_Th2,delta_pd1,delta_pd2,delta_ph1,delta_ph2,delta_hHR,delta_pdR,delta_phR,h1,h2,hG,hL,hMT,p1,p2,pR',',','Value,') 'Value'];
--- Codes/ModelicaPlant/estimatortest.m
+++ Codes/ModelicaPlant/estimatortest.m
@@ -1,4 +1,5 @@
 clearvars
+format long
 % Initializing FMIKit and adding paths
 FMIKit.initialize
 addpath('C:\Users\u375749\Documents\Thesis\Codes\Linearization')
@@ -13,19 +14,59 @@ kfinit_simulink
 
 load uy_sim
 
+feedback = 1; % feedback of estimator
 U = uy_sim.signals.values(:,1:nu);
 Y = uy_sim.signals.values(:,nu+1:nu+ny);
-u = U - initial.us';
-y = Y - (kf.C*initial.xs + kf.D*initial.us)';
+% u = U - initial.us';
+% y = Y - (kf.C*initial.xs + kf.D*initial.us)';
 uy = [zeros(nu+ny,1); reshape(system.A,nx*nx,1); reshape(system.B,nx*nu,1); reshape(system.C,ny*nx,1); reshape(system.D,ny*nu,1)];
-for it = 1000:2000
+ABCD = reshape([system.A system.B; system.C system.D],(nx+ny)*(nx+nu),1);
+Xs = initial.xs;
+finish = 2000;
+start = 201;
+record = zeros(nx,finish-start+1);
+for it = start:finish
+    if ~rem(it,100)
+        disp(['Iteration ' num2str(it)])
+    end
     xf = kf.markovPredictor(uy(1:nu),uy(nu+1:nu+ny),reshape(uy(nu+ny+1:nu+ny+nx*nx),nx,nx),reshape(uy(nu+ny+nx*nx+1:nu+ny+nx*nx+nx*nu),nx,nu),reshape(uy(nu+ny+nx*nx+nx*nu+1:nu+ny+nx*nx+nx*nu+ny*nx),ny,nx), reshape(uy(nu+ny+nx*nx+nx*nu+ny*nx+1:nu+ny+nx*nx+nx*nu+ny*nx+ny*nu),ny,nu));
-    Xf = xf + initial.xs;
-    ux = [u(it,:)'+initial.us; xf+initial.xs];
-    ABCD = LTVsystemDescription(ux(1:nu),ux(nu+1:nu+nx));
+    Xs = xf + Xs;%E = kf.C*xf + kf.D*u(it,:)' + Xf;
+    UXY = [U(it,:)'; Xs; Y(it,:)'];
+    ABCD = LTVsystemDescription(UXY(1:nu), UXY(nu+1:nu+nx), UXY(nu+nx+1:nu+nx+ny),feedback);
     A = ABCD(1:nx*nx);
     B = ABCD(nx*nx+1:nx*nx+nx*nu);
     C = ABCD(nx*nx+nx*nu+1:nx*nx+nx*nu+ny*nx);
     D = ABCD(nx*nx+nx*nu+ny*nx+1:nx*nx+nx*nu+ny*nx+ny*nu);
-    uy = [u(it,:)'; y(it,:)'; A; B; C; D];
+    u = U(it,:)' - U(it-1,:)'; % Is it right? TODO
+    y = Y(it,:)' - Y(it-1,:)';
+    uy = [u; y; A; B; C; D];
+    record(:,it-start+1) = Xs;
 end
+t = start:Ts:finish;
+figure(1)
+clf
+subplot(221)
+hold on
+plot(t,record(2,:))
+plot(t,record(7,:))
+plot(t,record(13,:))
+hold off
+subplot(222)
+hold on
+plot(t,record(3,:))
+plot(t,record(8,:))
+plot(t,record(14,:))
+plot(t,record(17,:))
+hold off
+subplot(223)
+hold on
+plot(t,record(5,:)-273)
+plot(t,record(10,:)-273)
+hold off
+subplot(224)
+hold on
+plot(t,record(6,:))
+plot(t,record(12,:))
+plot(t,record(16,:))
+plot(t,record(18,:))
+hold off
